a26acb1979c7317e5e817940be918c1cb371716b
Eduard Golovash <myausek@gmail.com>
2024-03-14 16:05:10 +0300

    Ревью правки users

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -9,7 +9,6 @@ router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('auth/token/logout/',
