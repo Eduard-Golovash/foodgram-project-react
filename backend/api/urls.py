79422797570e48d3680b2e6f5724ce617a9e787a
Eduard Golovash <myausek@gmail.com>
2024-03-14 03:32:48 +0300

    Ревью правки users

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from backend.api.views import (
+from api.views import (
     IngredientsViewSet,
     TagsViewSet,
     RecipesViewSet
