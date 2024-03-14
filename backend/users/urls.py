cff478af47ea5b44edc0ed7a27f1c0c8956f8061
Eduard Golovash <myausek@gmail.com>
2024-03-14 16:27:05 +0300

    Ревью правки users

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -10,7 +10,7 @@ router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/', include('djoser.urls')),
+    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('auth/token/logout/',
          TokenDestroyView.as_view(), name='token_destroy'),
