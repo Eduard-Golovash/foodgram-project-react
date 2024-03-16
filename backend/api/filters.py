a8158ffa261d965ee5dc0a5ac188b0f8ff15cf2a
Eduard Golovash <myausek@gmail.com>
2024-03-16 17:43:20 +0300

    Правки избранного/списка покупок

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,11 @@
 from django_filters import FilterSet, filters
-from recipes.models import Recipe, Ingredient, Tag
+from recipes.models import (
+    Recipe,
+    Ingredient,
+    Tag,
+    Favorite,
+    ShoppingList
+)
 
 
 class IngredientFilter(FilterSet):
@@ -17,20 +23,27 @@ class RecipeFilter(FilterSet):
         queryset=Tag.objects.all(),
     )
 
+    is_favorited = filters.BooleanFilter(
+        method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart')
+
     class Meta:
         model = Recipe
         fields = ('tags', 'author',)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        user = getattr(self.request, 'user', None)
-        if user and user.is_authenticated:
-            self.filters['is_favorited'] = filters.BooleanFilter(
-                method='filter_is_favorited', label='Is Favorited'
-            )
-
     def filter_is_favorited(self, queryset, name, value):
         user = self.request.user
         if value and not user.is_anonymous:
-            return queryset.filter(favorites__user=user)
+            favorite_recipes = Favorite.objects.filter(
+                user=user).values_list('recipe_id', flat=True)
+            return queryset.filter(id__in=favorite_recipes)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value and not user.is_anonymous:
+            shopping_list_recipes = ShoppingList.objects.filter(
+                user=user).values_list('recipe_id', flat=True)
+            return queryset.filter(id__in=shopping_list_recipes)
         return queryset
