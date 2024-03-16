119a2f0972eb2ab73abc53289707187186909e96
Eduard Golovash <myausek@gmail.com>
2024-03-16 18:47:18 +0300

    Правки избранного/списка покупок

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -13,11 +13,9 @@ class IngredientFilter(FilterSet):
         fields = ['name']
 
 
-class RecipeFilter(FilterSet):
-    is_favorited = filters.BooleanFilter(
-        method='filter_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
-        method='filter_is_in_shopping_cart')
+class RecipeFilter(filters.FilterSet):
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(method='filter_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
@@ -26,11 +24,11 @@ class RecipeFilter(FilterSet):
     def filter_is_favorited(self, queryset, name, value):
         user = self.request.user
         if value and user.is_authenticated:
-            return queryset.filter(favorites__user=user)
+            return queryset.filter(favorite__user=user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
         if value and user.is_authenticated:
-            return queryset.filter(shopping_lists__user=user)
+            return queryset.filter(shoppinglist__user=user)
         return queryset
