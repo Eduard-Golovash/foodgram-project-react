264d759e845048e4404fb2d9c6deb26b1b84cc59
Eduard Golovash <myausek@gmail.com>
2024-03-16 22:26:16 +0300

    Правки избранного/списка покупок

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,7 +1,8 @@
 from django_filters import FilterSet, filters
 from recipes.models import (
     Recipe,
-    Ingredient
+    Ingredient,
+    Tag
 )
 
 
@@ -14,23 +15,28 @@ class IngredientFilter(FilterSet):
 
 
 class RecipeFilter(FilterSet):
-    is_favorited = filters.BooleanFilter(
-        method='filter_is_favorited')
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
+    )
+
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
-        fields = ['is_favorited', 'is_in_shopping_cart', 'tags', 'author']
+        fields = ('tags', 'author',)
 
     def filter_is_favorited(self, queryset, name, value):
         user = self.request.user
-        if value and user.is_authenticated:
+        if value and not user.is_anonymous:
             return queryset.filter(favorite__user=user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
-        if value and user.is_authenticated:
+        if value and not user.is_anonymous:
             return queryset.filter(shoppinglist__user=user)
         return queryset
