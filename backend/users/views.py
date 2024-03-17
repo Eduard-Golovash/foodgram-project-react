013f2a46011a8dfa9742a152e978c737b45c5d81
Eduard Golovash <myausek@gmail.com>
2024-03-17 19:19:37 +0300

    Корректировка метода подписки/отписки

--- backend/users/views.py
+++ backend/users/views.py
@@ -70,31 +70,37 @@ class UserViewSet(viewsets.ModelViewSet):
                                             context={'request': request})
         return self.get_paginated_response(serializer.data)
 
-    @staticmethod
-    def add_to_list(model, user, author):
-        if model.objects.filter(user=user, author=author).exists():
-            return Response({'error': 'Вы уже подписаны на этого пользователя'},
-                            status=status.HTTP_400_BAD_REQUEST)
-        model.objects.create(user=user, author=author)
-        return Response({'message': 'Подписка успешно создана'},
-                        status=status.HTTP_201_CREATED)
-
-    @staticmethod
-    def remove_from_list(model, user, author):
-        obj = model.objects.filter(user=user, author=author)
-        if obj.exists():
-            obj.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response({'errors': 'Подписка не существует'},
-                        status=status.HTTP_400_BAD_REQUEST)
-
     @action(detail=True, methods=['post'],
             permission_classes=(IsAuthenticated,))
     def subscribe(self, request, **kwargs):
         author = get_object_or_404(User, id=kwargs['pk'])
-        return self.add_to_list(Subscription, request.user, author)
+        serializer = SubscriptionActionSerializer(
+            data={'author': author}, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        subscription, created = Subscription.objects.get_or_create(
+            user=request.user, author=author)
+        if not created:
+            return Response(
+                {'error': 'Вы уже подписаны на этого пользователя',
+                 'is_subscribed': True},
+                status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {'message': 'Подписка успешно создана', 'is_subscribed': True},
+            status=status.HTTP_201_CREATED
+        )
 
     @subscribe.mapping.delete
     def delete_subscribe(self, request, **kwargs):
         author = get_object_or_404(User, id=kwargs['pk'])
-        return self.remove_from_list(Subscription, request.user, author)
+        subscription = Subscription.objects.filter(
+            user=request.user, author=author).first()
+        if not subscription:
+            return Response({'errors': 'Подписка не существует',
+                             'is_subscribed': False},
+                            status=status.HTTP_400_BAD_REQUEST)
+        subscription.delete()
+        serializer = SubscriptionActionSerializer(
+            author, context={'request': request})
+        data = serializer.data
+        data['is_subscribed'] = False
+        return Response(data, status=status.HTTP_204_NO_CONTENT)
